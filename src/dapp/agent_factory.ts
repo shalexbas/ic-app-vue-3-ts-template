b706bb87d9ffb7e42b2ad7ca485a44b697eacca1
shalexbas <shalexbas@gmail.com>
2021-09-22 16:20:52 +0200

    ref

--- src/dapp/agent_factory.ts
+++ src/dapp/agent_factory.ts
@@ -1,4 +1,4 @@
-import { createActor as counterCreateActor } from "../declarations/counter";
+import { createActor as counterCreateActor } from "@/../declarations/counter";
 
 export const counterCanisterId = import.meta.env.VITE_APP_COUNTER_CANISTER_ID;
 
